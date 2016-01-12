c6146b6c0c82de8ca836e5c9a1d04cfc6877ced9
Paul Talaga <paul.talaga@uc.edu>
2016-01-12 09:38:52 -0500

    Week 1 Tues

---
+++ Week1-PGT-hello-int-loop/hello-world.cpp
@@ -0,0 +1,13 @@
+#include <iostream>
+
+using namespace std;
+
+int main(){
+  
+  string name;
+  cout << "What is your name?";
+  cin >> name;
+  
+  cout << "Hello " << name << endl;
+  return 0;
+}
---
+++ Week1-PGT-hello-int-loop/int.cpp
@@ -0,0 +1,13 @@
+#include <iostream>
+
+using namespace std;
+
+int main(){
+  
+  int number = 0;
+  while(1){
+    cout << "Number: " << number << endl;
+    number = number + 100000;
+  }
+  return 0;
+}
