c15e5337465efc0a3d4da6205f2a5558a7a95a93
Paul Talaga <paul.talaga@uc.edu>
2016-01-20 09:02:29 -0500

    Style demo cpp

---
+++ Week2-PGT/style.cpp
@@ -0,0 +1,38 @@
+#include <iostream>
+
+using namespace std;
+
+const float PI = 3.1415; 
+
+void dumbFunction(){
+
+}
+
+int main(){
+
+  int b = 6;
+  int c = 10;
+  
+  int this_is_a_long_variable;
+ 
+  if(stuff){
+    lots of stuff in here
+  }
+ 
+  if(b == c){ 
+    cout << "the same!\n";
+    cout << "the same again\n";
+  }else{
+    cout << "not the same!\n";
+  }
+  
+/*  Don't do this
+  if(b == c)
+    cout << "the same!\n";
+    cout << "This is something else\n";
+  else
+    cout << "not the same!\n";
+*/ 
+    
+  return 0;
+}
