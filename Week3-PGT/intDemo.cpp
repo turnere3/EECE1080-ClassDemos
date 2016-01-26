40a210299821351cbca33c160fda07a43fd14ff3
Paul Talaga <paul.talaga@uc.edu>
2016-01-26 10:03:26 -0500

    Talaga Class Demos from Jan 26

---
+++ Week3-PGT/intDemo.cpp
@@ -0,0 +1,10 @@
+#include <iostream>
+
+using namespace std;
+
+int main(){
+  int a;
+  
+  cout << "a: " << a << endl;// EEk, random data!
+  // Be sure to always initialize variables.
+}
---
+++ Week3-PGT/nested-loops.cpp
@@ -0,0 +1,12 @@
+#include <iostream>
+
+using namespace std;
+
+int main(){
+  for(int y = 0; y < 10; y++){
+    for(int x = 0; x < 10; x++){
+      cout << 'x';
+    }
+    cout << endl;
+  }
+}
