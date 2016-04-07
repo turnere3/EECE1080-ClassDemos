484e4da6aea297f376127d5554c69fedcd0526a8
Paul Talaga <paul.talaga@uc.edu>
2016-04-07 10:44:47 -0400

    pointers to pointers!

--- Week13-PGT-Pointers-GDB/crash.cpp
+++ Week13-PGT-Pointers-GDB/crash.cpp
@@ -4,25 +4,53 @@ using namespace std;
 
 int main(){
 
+  /*int* bob = NULL;
+  
+  //cout << *bob << endl;
+  //return 0;
+  
+  for(unsigned i = 0; i < 100000000; i++){
+     bob = new int;
+     *bob = 45;
+     delete bob;
+     bob = NULL;
+     //cout << *bob << endl;
+  }
+  //*bob = 5;
+  
+  
+  cout << *bob << "-" << bob << endl;
+  */
+  
+  /*
+  int* bob;
+  int x = 5;
+  bob = &x;
+  
+  */
+  
+  /*
+  int x = 5;
   int** bob = new int*;
+  *bob = &x;
+
+  cout << x << "-" << *bob << "-" << **bob << endl;
   
-  //int x = 5;
+  */
   
-  //*bob = &x;
-  
-  //*bob = 50;
-  
+  /*
+  int** bob = new int*;
   *bob = new int;
+  **bob = 94;
   
-  **bob = 50;
-
-  cout << **bob << "-" << *bob << "-" << bob <<endl;
+  cout << **bob << endl;
   
-  delete bob;
-  bob = NULL;
-  cout << bob << endl;
-  delete *bob;
-  //delete bob;
-
+  */
+  
+  void** bob = new void*;
+  *bob = (void*)(new int);
+  
+  *(int*)(*bob) = 5;
+  
   return 0;
 }
