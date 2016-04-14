5d161e250e33fef6dfae58d603ddfcd1b9d33fb6
Paul Talaga <paul.talaga@uc.edu>
2016-04-14 09:55:51 -0400

    Smart pointer description

--- Week14-PGT-LL-Recursion/smartPtr.cpp
+++ Week14-PGT-LL-Recursion/smartPtr.cpp
@@ -1,3 +1,12 @@
+/*
+  A quick Linked list implementation using smart-pointers.  These smart pointers
+  do reference counting to self-destruct when needed (like Java).  Thus, no deletes
+  are needed.
+  
+  MUST be compiled with c++11 extensions:  g++ -std=c++11 smartPtr.cpp
+  
+  Paul Talaga April 2016
+*/
 #include <iostream>
 #include <memory>
 
@@ -5,7 +14,7 @@ using namespace std;
 
 struct node_t{
   int data;
-  shared_ptr<node_t> next;
+  shared_ptr<node_t> next;  // Rather than a node_t*, this is a 'smart' pointer to a node_t
 };
 
 void print(shared_ptr<node_t> node){
@@ -17,8 +26,8 @@ void print(shared_ptr<node_t> node){
 }
 
 shared_ptr<node_t> prepend(shared_ptr<node_t> node, int value){
-  shared_ptr<node_t> head(new node_t);
-  head->next = node;
+  shared_ptr<node_t> head(new node_t);  // Essentially a 'smart' pointer is a wrapper
+  head->next = node;                    // around regular pointers.
   head->data = value;
   return head;
 }
