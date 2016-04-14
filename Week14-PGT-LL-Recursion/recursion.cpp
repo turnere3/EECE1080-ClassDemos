47f97ccf1a3660560ba16e030962b3d3ec1684c0
Paul Talaga <paul.talaga@uc.edu>
2016-04-14 09:48:45 -0400

    Fixed recursion.cpp so it works

--- Week14-PGT-LL-Recursion/recursion.cpp
+++ Week14-PGT-LL-Recursion/recursion.cpp
@@ -1,25 +1,39 @@
+/*
+  Demo of multiple ways of summing up numbers in an array.  3 variants of recursion, and
+  one loop method.  All have slightly different speeds.
+  Paul Talaga April 2016
+*/
+
 #include <iostream>
 
 using namespace std;
 
-int sum(int l[], int index, int length){  // From small i to larger i
+// Simple way to do sum recursively.  Going up from 0 and stopping at the
+// end.  Requires 2 integer variables.  Slowest.
+int sum(int l[], int index, int length){  
   if( index == length) return 0;
   return sum(l, index + 1, length) + l[index];
 }
 
+// Simple way to do sum, but go down from the top.
+// Requires one integer variable.  Slightly faster.
 int sum2(int l[], int index){  // From large i to small i
   if( index == 0) return 0;
   return sum2(l, index - 1 ) + l[index-1];
 }
 
-
-// Compile with g++ -O3 to make this faster.  It turns into a loop. 
-void sum3(int l[], int index, int& ret){  // From large i to small i, no return
-  //if( index == 0) return;
-  ret = ret + l[0];
-  return sum3(l, index - 1, ret );      // Tail recursive
+// Most complex way.  Still one variable is used, but we 'return' the answer
+// via a reference variable.
+// If compiled with g++ -03  (full optimizations) the compiler can convert this into
+// a loop and not make any new stack frames.
+// Fastest recursive way to do sum.  As fast as a loop.
+void sum3(int l[], int index, int& ret){
+  if( index == 0) return;
+  ret = ret + l[index-1];
+  return sum3(l, index - 1, ret );      // Tail recursive!  No work after recursive call.
 }
 
+// The boring way with a loop.
 int sum4(int l[], int length){
   int count = 0;
   for(int i = 0; i < length; i++){
@@ -31,23 +45,28 @@ int sum4(int l[], int length){
 int main(){
   
   const int SIZE = 100000;
-  int bob[SIZE] = {};
+  int bob[SIZE];
+  for(unsigned i = 0; i < SIZE; i++){
+    bob[i] = i % 100;
+  }
   
   clock_t start = clock();
   cout << sum(bob,0,SIZE) << endl;
-  cout << "time: " << clock() - start << endl;
+  cout << "sum  time: " << clock() - start << endl;
+  
   start = clock();
   cout << sum2(bob,SIZE) << endl;
-  cout << "time: " << clock() - start << endl;
+  cout << "sum2 time: " << clock() - start << endl;
+  
   start = clock();
   int ret = 0;
   sum3(bob,SIZE, ret);
   cout << ret << endl;
-  cout << "time: " << clock() - start << endl;
-  start = clock();
+  cout << "sum3 time: " << clock() - start << endl;
   
+  start = clock();
   cout << sum4(bob,SIZE) << endl;
-  cout << "time: " << clock() - start << endl;
+  cout << "loop time: " << clock() - start << endl;
   start = clock();
 
   return 0;
